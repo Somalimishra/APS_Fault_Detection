eecd21be8ea8fc2953ab7cc00e54a590bc74e3dd
Somalimishra <somali.mishra2016@gmail.com>
2022-11-29 22:19:49 +0530

    removed one blank space from main.py file

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import pymongo
 
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-
 # Database Name
-dataBase = client["neurolabDB"]
+dataBase = client["neurolabDB"] 
 
 # Collection  Name
 collection = dataBase['Products']
